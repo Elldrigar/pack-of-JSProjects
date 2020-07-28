216129beb44b2022dadeb65081022c25086cd8a4
Artur Gawron <artur@gawron.me>
2020-07-28 19:48:55 +0200

    Add Quote from API to HTML

--- 01_Quote-generator/script.js
+++ 01_Quote-generator/script.js
@@ -1,3 +1,11 @@
+// const quoteContainer = documnet.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+
 // GET QUOTE FROM FORISMATIC API
 async function getQuote() {
     const proxyURL = 'https://intense-tor-03895.herokuapp.com/'
@@ -6,8 +14,20 @@ async function getQuote() {
     try {
         const response = await fetch(proxyURL + apiURL)
         const data = await response.json()
-        console.log(data)
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //REDUCE FONT SIZE FOR LONG QUOTES
+        if (data.quoteText.length > 100) {
+            quoteText.classList.add('quote__text--long')
+        } else {
+            quoteText.classList.remove('quote__text--long')
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
+        getQuote()
         console.log('Error', error)
     }
 }
