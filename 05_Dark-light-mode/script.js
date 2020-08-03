4f14e92c98726222ef5982ee8c15c1b6af61313f
Artur Gawron <artur@gawron.me>
2020-08-03 22:26:43 +0200

    Add light/dark style for nav,textBox and toggleIcon

--- 05_Dark-light-mode/script.js
+++ 05_Dark-light-mode/script.js
@@ -6,11 +6,29 @@ const imageTwo = document.getElementById('image-2');
 const imageThree = document.getElementById('image-3');
 const textBox = document.querySelector('.text__box');
 
+//DARK THEME
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+}
+
+//LIGHT THEME
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
   }
 }
 
