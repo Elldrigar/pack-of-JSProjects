1da01dbff736c02133668afd87e8d6a6fc8a3c73
Artur Gawron <artur@gawron.me>
2020-08-03 15:54:46 +0200

    Grab all elements to variables

--- 05_Dark-light-mode/script.js
+++ 05_Dark-light-mode/script.js
@@ -1,4 +1,10 @@
 const toggleSwitch = document.querySelector('.theme-switch__input');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const imageOne = document.getElementById('image-1');
+const imageTwo = document.getElementById('image-2');
+const imageThree = document.getElementById('image-3');
+const textBox = document.querySelector('.text__box');
 
 function switchTheme(event) {
   if (event.target.checked) {
