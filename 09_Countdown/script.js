46f4d0492d65a45aed723dab95f5be0a88b03c7b
Artur Gawron <artur@gawron.me>
2020-08-14 15:38:42 +0200

    Take values from inputs

--- 09_Countdown/script.js
+++ 09_Countdown/script.js
@@ -2,6 +2,9 @@ const inputContainer = document.getElementById('container-input');
 const countdownForm = document.getElementById('countdownForm');
 const dateElement = document.getElementById('date');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // SET DATE INPUT MIN WITH TODAY'S DATE
 const today = new Date().toISOString().split('T')[0];
 dateElement.setAttribute('min', today);
@@ -9,7 +12,10 @@ dateElement.setAttribute('min', today);
 // TAKE VALUES FROM FORM INPUT
 function updateCountdown(event) {
   event.preventDefault();
-  console.log(event);
+  countdownTitle = event.srcElement[0].value;
+  countdownDate = event.srcElement[1].value;
+  console.log(countdownTitle);
+  console.log(countdownDate);
 }
 
 // EVENT LISTENERS
