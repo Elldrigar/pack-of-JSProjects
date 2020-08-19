9b2aae3e783996d63c9a0eb5ccff912a0bd7b54e
Artur Gawron <artur@gawron.me>
2020-08-19 21:21:40 +0200

    Add validate form

--- 10_bookmark/script.js
+++ 10_bookmark/script.js
@@ -12,6 +12,38 @@ function showModal() {
   websiteNameElement.focus();
 }
 
+// VALIDATE FORM
+function validate(nameValue, urlValue) {
+  const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('Proszę uzupełnij obydwa pola!');
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Proszę podaj prawidłowy adres www.');
+    return false;
+  }
+  return true;
+}
+
+//HANDLE FORM
+
+function storeBookmark(event) {
+  event.preventDefault();
+  const nameValue = websiteNameElement.value;
+  let urlValue = websiteUrlElement.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+
+  //   validate(nameValue, urlValue);
+}
+
 // EVENT LISTENER
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () =>
@@ -20,3 +52,4 @@ modalClose.addEventListener('click', () =>
 window.addEventListener('click', (event) =>
   event.target === modal ? modal.classList.remove('show-modal') : false
 );
+bookmarkForm.addEventListener('submit', storeBookmark);
