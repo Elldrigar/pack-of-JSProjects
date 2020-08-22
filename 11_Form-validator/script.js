78c4073b114fbfed33f63b1228daf0ca2e029468
Artur Gawron <artur@gawron.me>
2020-08-22 21:35:20 +0200

    Add validateForm using checkValidity()

--- 11_Form-validator/script.js
+++ 11_Form-validator/script.js
@@ -4,8 +4,15 @@ const password2Element = document.getElementById('password2');
 const messagecontainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
+let isValid = false;
+
+function validateForm() {
+  isValid = form.checkValidity();
+}
+
 function procesFormData(event) {
   event.preventDefault();
+  validateForm();
 }
 
 // EVENT LISTENER
