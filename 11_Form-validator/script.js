812e2f8d079757cd857a7cc026d1c19ed052de49
Artur Gawron <artur@gawron.me>
2020-08-23 22:23:19 +0200

    Add validation for both passwords

--- 11_Form-validator/script.js
+++ 11_Form-validator/script.js
@@ -3,15 +3,38 @@ const password1Element = document.getElementById('password1');
 const password2Element = document.getElementById('password2');
 const messagecontainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
+const colorRed = '#db1717';
+const colorGreen = '#0bda0b';
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   isValid = form.checkValidity();
   if (!isValid) {
-    message.textContent = 'Please fill out all fields!';
-    message.style.color = '#db1717';
-    messagecontainer.style.borderColor = '#db1717';
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = colorRed;
+    messagecontainer.style.borderColor = colorRed;
+    return;
+  }
+
+  if (password1Element.value === password2Element.value) {
+    passwordsMatch = true;
+    password1Element.style.borderColor = colorGreen;
+    password2Element.style.borderColor = colorGreen;
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = colorRed;
+    messagecontainer.style.borderColor = colorRed;
+    password1Element.style.borderColor = colorRed;
+    password2Element.style.borderColor = colorRed;
+    return;
+  }
+  if (isValid && passwordsMatch) {
+    message.textContent = 'Successfully Registered!';
+    message.style.color = colorGreen;
+    messagecontainer.style.borderColor = colorGreen;
   }
 }
 
